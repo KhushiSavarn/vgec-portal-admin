07fa5f609823e06b728e08b2d335c56fcaaaff2a
Ujjwal-TST <127100580+Ujjwal-TST@users.noreply.github.com>
2023-07-19 17:44:42 +0530

    add authentic route

--- src/component/layout/AppLayout.js
+++ src/component/layout/AppLayout.js
@@ -50,6 +50,7 @@ const AppLayout = () => {
   const api = useHttp();
   useEffect(() => {
     if (!(getAuthToken() !== undefined && getAuthToken() !== null)) {
+      navigate(loginRoot)
       return;
     }
     if (!CONSTANTS.GETMe) {
